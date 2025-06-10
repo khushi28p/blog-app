6001446d1cfbe942db3c6bb0dde9b162c0961522
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-06-10 10:14:35 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -11,18 +11,13 @@ app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected successfully!');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-  });
 
 app.get("/", (req, res) => {
-    res.send(`Server running on port ${PORT}`);
+    res.send("Server running");
+})
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 })
