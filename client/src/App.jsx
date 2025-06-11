962cab62c85db73bb2b1b1a39aac30e48ad0797e
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-06-11 17:50:00 +0530

    minor changes to frotnend:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <p className='text-8xl font-bold text-green-500'>Working...</p>
+      <Button className="bg-blue-400 text-black">Click me</Button>
     </>
   )
 }
