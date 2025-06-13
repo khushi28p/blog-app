6d5b2666c83774afe58e1a1c07263fcec544d9b2
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-06-13 14:39:47 +0530

    persist aded

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,19 +4,22 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
 import { loadUserFromLocalStorage } from './redux/authSlice.js'
 import { Toaster } from "sonner"
+import PersistGate from 'redux-persist/integration/react'
 
 store.dispatch(loadUserFromLocalStorage());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <App />
     <Toaster richColors position="top-center" expand={true} />
     </BrowserRouter>
+    </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 import authReducer from "./authSlice.js";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+const persistConfig = {
+    key:'root',
+    storage
+}
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer:{
-         auth: authReducer,
-    }
+    reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store);
